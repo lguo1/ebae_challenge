87054c6eb79e6c02a5d082aff8c857aa124041cc
lguoterry <lguoterry@gmail.com>
2020-09-15 10:37:34 -0400

    porco

--- parser.py
+++ parser.py
@@ -3,5 +3,5 @@ from attributes import *
 
 pd.options.display.max_colwidth = 20
 df = pd.read_csv("mlchallenge_set_2021.tsv", header=None, sep='\t', names=['category','primary_image','all_image','attributes','index'])
-df['attributes']=df['attributes'].apply(lambda x: attribute_parser(x))
+df['attributes']=df['attributes'].apply(attribute_parser)
 print(df.head())

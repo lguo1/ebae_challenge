6d79256723731afe516ad0c8ff945b60ae257276
lguoterry <lguoterry@gmail.com>
2020-09-15 03:57:48 -0400

    works?

--- attributes.py
+++ attributes.py
@@ -2,36 +2,28 @@
 
 def attribute_parser(string):
   #remove outer parentheses
+  if string == '()': return {}
   string1 = string[1:len(string)-1]
   
   #split at ':', remove trailing whitespace and empty strings from more than one ':'
-  string2 = list(map(lambda x:x.strip(), filter(lambda x:x != "", string1.split(":"))))
+  string2 = list(map(lambda x:x.strip(), filter(lambda x:x != '', string1.split(":"))))
 
-  
   #create list of [non-last items, last item] pairs
-  string3 = []
-
-  string3.append([[], string2[0]])
-
+  string3 = [[[], string2[0]]]
   for thing in string2[1: len(string2)-1]:
     thing1 = thing.split(",")
     string3.append([thing1[:len(thing1)-1], thing1[len(thing1)-1]])
-
   string3.append([[string2[len(string2)-1]],[]])
   
   #pair last item from one with non-last items from the next
   string4 = []
-
   for i in range(len(string3)-1):
     string4.append([string3[i][1], string3[i+1][0]])
   
   #make dictionary
   string5 = {}
   for thing in string4:
-    string5[thing[0]] = ",".join(thing[1])
+    string5[thing[0].strip()] = ",".join(thing[1])
 
   return string5
-
-
-print(attribute_parser("(Colors:blue, white,Special Note::very nice,Style: Modern)"))
 
--- parser.py
+++ parser.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from attributes import *
+
 pd.options.display.max_colwidth = 20
-df = pd.read_csv("mlchallenge_set_2021.tsv", header=None, sep='\t')
+df = pd.read_csv("mlchallenge_set_2021.tsv", header=None, sep='\t', names=['category','primary_image','all_image','attributes','index'])
+df['attributes']=df['attributes'].apply(lambda x: attribute_parser(x))
 print(df.head())

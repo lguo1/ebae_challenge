b66fbaa75c596cdb0f146bd7a34fe94d70c0b7ff
lguoterry <lguoterry@gmail.com>
2020-09-15 01:36:09 -0400

    yo

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
 import pandas as pd
 pd.options.display.max_colwidth = 20
-df = pd.read_csv("mlchallenge_set_2021.tsv", header=None, sep='\t')
+df = pd.read_csv("yy.tsv", header=None, sep='\t')
 print(df.head())
